c4f1a4b92cd53b8ae039df1fec8a981b18c0cf16
aleksandr-suchugov <alex.coup@yandex.ru>
2024-04-20 17:18:44 +0300

    middleware isAdmin added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\GroupStudentController;
@@ -9,6 +10,14 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::resource('groups', GroupController::class);
-Route::resource('students', StudentController::class);
-Route::resource('groups.students', GroupStudentController::class);
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::resource('groups', GroupController::class);
+    Route::resource('students', StudentController::class);
+    Route::resource('groups.students', GroupStudentController::class);
+
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
